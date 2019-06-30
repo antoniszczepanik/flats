97203787af8a1dfc9519151e8db5586d321f4d0d
Antoni <antoni.szczepanik@greensteam.com>
2019-06-30 22:52:32 +0200

    removed scripts

--- scripts/run_spider_sale.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Starting sale scraper ..."
-cd ~/repos/avm-v2
-_now=$(date +"%m_%d_%Y")
-_file_path="ftp://user:user@localhost:1821/Scraping_data/sale/sales_up_to_$_now.csv"
-scrapy crawl morizon -o $_file_path &&
-echo "Saved scraped sale data on GDrive as $_file_path";
-

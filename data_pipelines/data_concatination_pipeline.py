05a9c355f2cb71ecd0a9f0c6372d168513b6ca72
Antoni <antoni.szczepanik@greensteam.com>
2019-07-08 17:45:54 +0200

    fully functional concat pipeline

--- data_pipelines/data_concatination_pipeline.py
+++ data_pipelines/data_concatination_pipeline.py
@@ -11,7 +11,7 @@ import pandas as pd
 from s3_bucket import S3Bucket
 from utils import concat_csvs
 
-FOLDERS_TO_CONCAT = ['morizon_sale', 'morizon_rent']
+FOLDERS_TO_CONCAT = ['morizon_sale/raw', 'morizon_rent/raw']
 BUCKET_NAME = 'morizon-data'
 # use if you want to select key-pair from ~/.aws/config, else comment out
 S3_PROFLE = 's3-private'
@@ -33,15 +33,17 @@ if __name__ == '__main__':
             bucket = S3Bucket(bucket_name=BUCKET_NAME,
                               s3_dir_path=s3_folder,
                               local_downloads_dir=tmpdir,
+                              s3_target_dir_path=f'{s3_folder.split("/")[0]}/concated',
                               profile=S3_PROFLE)
             bucket.download_files()
             s3_paths = bucket.list_paths()
             local_paths = [f'{tmpdir}/{path.split("/")[-1]}' for path in s3_paths]
             concatinated_df = concat_csvs(local_paths)
 
-            current_datetime_str = datetime.now().strftime('%Y_%m_%dT%H_%M_%S')
-            full_filename = f'{s3_folder}_full_{current_datetime_str}.csv'
-            concatinated_df.to_csv(f'{tmpdir}/{full_filename}', index=False)
-            bucket.upload_file(f'{tmpdir}/{full_filename}', f'{s3_folder}_concated/{full_filename}')
+            current_dt = datetime.now().strftime('%Y_%m_%dT%H_%M_%S')
+            full_filename = f'{s3_folder.split("/")[0]}_full_{current_dt}.parquet'
+            concatinated_df.to_parquet(f'{tmpdir}/{full_filename}', index=False)
+            bucket.upload_file(f'{tmpdir}/{full_filename}',
+                               full_filename)
             log.info('Succesfully uploaded full file')
     log.info('Finished data_concatination pipeline.')

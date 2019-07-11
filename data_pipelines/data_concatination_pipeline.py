8a28f076bba6cbd99d6db6a215997e0bbe016fc0
kabuboy <szczepanik.antoni@gmail.com>
2019-07-11 21:42:32 +0200

    checking for previously downloaded s3 files

--- data_pipelines/data_concatination_pipeline.py
+++ data_pipelines/data_concatination_pipeline.py
@@ -9,36 +9,64 @@ import tempfile
 import logging as log
 import pandas as pd
 from s3_bucket import S3Bucket
-from utils import concat_csvs
+import utils
 
 FOLDERS_TO_CONCAT = ['morizon_sale/raw', 'morizon_rent/raw']
 BUCKET_NAME = 'morizon-data'
-# use if you want to select key-pair from ~/.aws/config, else comment out
-S3_PROFLE = 's3-private'
+ALREADY_CONCATED_FILES_LIST = '{spider_name}_concated_files_list.txt'
+S3_TARGET_DIR = '{spider_name}/concated'
+# which key-pair s3 value to use
+# S3_PROFLE = 's3-private'
+S3_PROFILE = 'default'
 
-# save logs to a file (later displayed on server, hopefully)
-#datetime = datetime.now().strptime()
-#log.basicConfig(filename='{s3_folder_name}_cleaning_{datetime}.log',
-                    #format='%(name)s - %(levelname)s - %(message)s')
-# log = logging.getLogger(__name__)
 log.basicConfig(level=log.INFO, format='%(asctime)s %(message)s',
                 datefmt='%m-%d-%Y %I:%M:%S')
-
 
 if __name__ == '__main__':
 
     log.info('Starting data concatination pipeline')
-    with tempfile.TemporaryDirectory() as tmpdir:
-        for s3_folder in FOLDERS_TO_CONCAT:
+    for s3_folder in FOLDERS_TO_CONCAT:
+        with tempfile.TemporaryDirectory() as tmpdir:
+            spider_name = s3_folder.split("/")[0]
+            s3_target_dir = S3_TARGET_DIR.format(spider_name=spider_name)
+            concated_files_list_path = ALREADY_CONCATED_FILES_LIST.format(spider_name=spider_name)
+
             bucket = S3Bucket(bucket_name=BUCKET_NAME,
                               s3_dir_path=s3_folder,
                               local_downloads_dir=tmpdir,
-                              s3_target_dir_path=f'{s3_folder.split("/")[0]}/concated',
-                              profile=S3_PROFLE)
-            bucket.download_files()
+                              s3_target_dir_path=s3_target_dir,
+                              profile=S3_PROFILE)
+
+            # Find paths different than in concated_files_list
             s3_paths = bucket.list_paths()
-            local_paths = [f'{tmpdir}/{path.split("/")[-1]}' for path in s3_paths]
-            concatinated_df = concat_csvs(local_paths)
+            try:
+                already_concated = utils.read_txt_list(concated_files_list_path)
+            except FileNotFoundError:
+                already_concated = []
+                log.info('Did not find already concated list')
+            else:
+                log.info('Found already concacted list.')
+            paths_to_concat = list(set(s3_paths) - set(already_concated))
+
+            utils.update_txt_list(paths_to_concat, concated_files_list_path)
+            log.info(f'Updated already concated list with {len(paths_to_concat)} new filepaths.')
+
+            for path in paths_to_concat:
+                bucket.download_file(path)
+            concated_files = bucket.list_paths(directory = s3_target_dir,
+                                                allowed_extension='.parquet')
+
+            previous_concated = utils.select_most_up_to_date_file(concated_files)
+            if len(previous_concated)>0:
+                log.info('Selected most up-to-date file from previously concated: %s', previous_concated)
+                bucket.download_file(previous_concated)
+            else:
+                log.info('Did not find previously concatinated files.')
+
+            local_paths = [f'{tmpdir}/{utils.name_from_path(path)}' for path in paths_to_concat]
+            local_concated_path = f'{tmpdir}/{utils.name_from_path(previous_concated)}'
+
+            concatinated_df = utils.concat_dfs(local_paths + [local_concated_path])
 
             current_dt = datetime.now().strftime('%Y_%m_%dT%H_%M_%S')
             full_filename = f'{s3_folder.split("/")[0]}_full_{current_dt}.parquet'

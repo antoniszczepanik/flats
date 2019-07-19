ea27868dd7705a8f930062bcaac0dc48ebd0053d
Ubuntu <ubuntu@ip-172-31-42-190.eu-central-1.compute.internal>
2019-07-19 20:35:20 +0000

    update setup to automaticaly deploy spider

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 sudo apt-get update
+# scrapyd installed via apt to ease administration work
 sudo apt-get -y install build-essential python3-dev python3-pip
-pip3 install -r requirements.txt
+sudo pip3 install -r requirements.txt
+export PATH=$PATH:~/.local/bin
+nohup scrapyd >/dev/null 2>&1 & 
+echo "Scrapyd server is up and running!"
 cd spider
-nohup scrapyd >/dev/null 2>&1 &
-
+scrapyd-deploy local-target -p morizon_spider

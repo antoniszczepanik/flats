01adcc2588e1df83a9fb364f05184155e39ba428
Ubuntu <ubuntu@ip-172-31-39-250.eu-central-1.compute.internal>
2019-07-19 22:52:35 +0000

    create setup dir

--- setup/setup.sh
+++ setup/setup.sh
@@ -3,14 +3,12 @@ apt-get -y install build-essential python3-dev python3-pip
 pip3 install -r requirements.txt
 export PATH=$PATH:~/.local/bin
 
-cd scrapyd
 nohup scrapyd >/dev/null 2>&1 & 
 echo "Scrapyd server is up and running!"
-cd ..
 
-cd spider
+cd ../spider
 scrapyd-deploy local-target -p morizon_spider
-cd ..
+cd ../setup
 
 crontab cron_jobs
 echo "Successfully added following jobs to schedule:"

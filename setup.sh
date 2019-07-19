48d52b72ebdb9c154be6c67d2e15328ce5108765
Ubuntu <ubuntu@ip-172-31-39-250.eu-central-1.compute.internal>
2019-07-19 22:28:23 +0000

    automate setting cronjobs

--- setup.sh
+++ setup.sh
@@ -1,9 +1,12 @@
-sudo apt-get update
-# scrapyd installed via apt to ease administration work
-sudo apt-get -y install build-essential python3-dev python3-pip
-sudo pip3 install -r requirements.txt
+apt-get update
+apt-get -y install build-essential python3-dev python3-pip
+pip3 install -r requirements.txt
 export PATH=$PATH:~/.local/bin
 nohup scrapyd >/dev/null 2>&1 & 
 echo "Scrapyd server is up and running!"
 cd spider
 scrapyd-deploy local-target -p morizon_spider
+cd ..
+crontab cron_jobs
+echo "Successfully added following jobs to schedule:"
+cat cron_jobs

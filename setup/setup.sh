ee40186e2f355c3a2c2eea1bbaf1f09b9e98a54e
kabuboy <szczepanik.antoni@gmail.com>
2019-07-20 15:37:20 +0200

    update readme

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,8 +1,10 @@
 apt-get update
 apt-get -y install build-essential python3-dev python3-pip
 pip3 install -r requirements.txt
+# add scrapyd etc to path
 export PATH=$PATH:~/.local/bin
 
+# run scrapyd server in background
 nohup scrapyd >/dev/null 2>&1 & 
 echo "Scrapyd server is up and running!"
 
@@ -13,3 +15,7 @@ cd ../setup
 crontab cron_jobs
 echo "Successfully added following jobs to schedule:"
 cat cron_jobs
+
+# create logs directories if dont exist
+mkdir -p logs/morizon_spider/morizon_rent
+mkdir -p logs/morizon_spider/morizon_sale

0d97e25dd081dc06d5fb107f941f3072586b40a1
Ubuntu <ubuntu@ip-172-31-38-95.eu-central-1.compute.internal>
2019-08-01 16:19:33 +0000

    complete setup for mounting s3

--- setup/setup.sh
+++ setup/setup.sh
@@ -2,7 +2,7 @@
 
 # install requirements
 apt-get update
-apt-get -y install build-essential python3-dev python3-pip
+apt-get -y install build-essential python3-dev python3-pip s3fs
 pip3 install -r requirements.txt
 # add scrapyd etc to path
 export PATH=$PATH:~/.local/bin
@@ -11,6 +11,8 @@ export PATH=$PATH:~/.local/bin
 nohup scrapyd >/dev/null 2>&1 & 
 echo "Scrapyd server is up and running!"
 
+# make sure in the right directory
+cd ~/morizon-spider/setup
 # deploy spider
 cd ../spider
 scrapyd-deploy local-target -p morizon_spider
@@ -22,9 +24,8 @@ echo "Successfully added following jobs to schedule:"
 cat cron_jobs
 
 # mount s3 in morizon-data
-apt-get install s3fs
 echo "Mounting s3 in ~/morizon-data"
 mkdir ~/morizon-data
 chmod 600 ~/.aws/credentials
-s3fs morizon-data ~/morizon-data -o passwd_file=/home/ubuntu/.aws/credentials -o use_cache=/tmp -o allow_other -o mp_umask=002
+s3fs morizon-data ~/morizon-data -o nonempty -o passwd_file=/home/ubuntu/.aws/credentials -o use_cache=/tmp -o allow_other -o mp_umask=077 -o uid=1000 -o gid=1000
 

f498c9552357673d885a273553f6aa4905cbe786
kabuboy <szczepanik.antoni@gmail.com>
2019-08-01 17:26:03 +0200

    modify setup

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,4 +1,6 @@
 # FIRST ADD ~/.aws/credentials file!!!
+
+# install requirements
 apt-get update
 apt-get -y install build-essential python3-dev python3-pip
 pip3 install -r requirements.txt
@@ -9,17 +11,20 @@ export PATH=$PATH:~/.local/bin
 nohup scrapyd >/dev/null 2>&1 & 
 echo "Scrapyd server is up and running!"
 
+# deploy spider
 cd ../spider
 scrapyd-deploy local-target -p morizon_spider
 cd ../setup
 
+# setup cron jobs
 crontab cron_jobs
 echo "Successfully added following jobs to schedule:"
 cat cron_jobs
 
+# mount s3 in morizon-data
 apt-get install s3fs
 echo "Mounting s3 in ~/morizon-data"
 mkdir ~/morizon-data
 chmod 600 ~/.aws/credentials
-s3fs morizon-data ~/morizon-data -o ~/.aws/credentials
+s3fs morizon-data ~/morizon-data -o passwd_file=~/.aws/credentials -o use_cache=/tmp
 

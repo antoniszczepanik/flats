4fa8d7740267b84b8e09aef754d8019b5142d9dc
antoniszczepanik <antoni.szczepanik@greensteam.com>
2019-10-17 16:09:39 +0200

    add dockerfile and moving stuff to deploy/

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,10 +5,10 @@ set -e
 
 # make sure in the correct directory
 REPO_ROOT=$(git rev-parse --show-toplevel)
-cd $REPO_ROOT/setup
+cd $REPO_ROOT/deploy
 
 # install requirements
-apt-get update
+apt-get update 
 apt-get -y install build-essential python3-dev python3-pip s3fs
 pip3 install -r requirements.txt
 
@@ -21,16 +21,23 @@ echo "Scrapyd server is up and running!"
 # deploy spider
 cd $REPO_ROOT/spider
 scrapyd-deploy local-target -p morizon_spider
-cd $REPO_ROOT/setup
+cd $REPO_ROOT/deploy
 
 # setup cron jobs
-crontab cron_jobs
-echo "Successfully added following jobs to schedule:"
+crontab cron_jobs && \
+echo "Successfully added following jobs to schedule:" && \
 cat cron_jobs
 
 # mount s3 in morizon-data
 echo "Mounting s3 in ~/morizon-data"
 mkdir -p ~/morizon-data
-s3fs morizon-data ~/morizon-data -o nonempty -o iam_role='S3_permission_for_ec2' -o use_cache=/tmp -o allow_other -o mp_umask=077 -o uid=1000 -o gid=1000
+s3fs morizon-data ~/morizon-data \
+	-o nonempty \
+	-o iam_role='S3_permission_for_ec2' \
+	-o use_cache=/tmp \
+	-o allow_other \
+	-o mp_umask=077 \
+	-o uid=1000 \
+	-o gid=1000
 
 

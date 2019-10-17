21475b899c32a56ddf1c41ab93212cf20e94616d
antoniszczepanik <antoni.szczepanik@greensteam.com>
2019-10-17 15:03:32 +0200

    update setup script

--- utils/setup.sh
+++ utils/setup.sh
@@ -1,4 +1,11 @@
-cd /home/ubuntu/flats/setup
+#!usr/bin/env bash
+
+# stop executing commands on failure
+set -e
+
+# make sure in the correct directory
+REPO_ROOT=$(git rev-parse --show-toplevel)
+cd $REPO_ROOT/setup
 
 # install requirements
 apt-get update
@@ -7,15 +14,14 @@ pip3 install -r requirements.txt
 
 # add scrapyd etc to path
 export PATH=$PATH:~/.local/bin
-
 # run scrapyd server in background
 nohup scrapyd >/dev/null 2>&1 & 
 echo "Scrapyd server is up and running!"
 
 # deploy spider
-cd ../spider
+cd $REPO_ROOT/spider
 scrapyd-deploy local-target -p morizon_spider
-cd -
+cd $REPO_ROOT/setup
 
 # setup cron jobs
 crontab cron_jobs
@@ -24,7 +30,7 @@ cat cron_jobs
 
 # mount s3 in morizon-data
 echo "Mounting s3 in ~/morizon-data"
-mkdir ~/morizon-data
+mkdir -p ~/morizon-data
 s3fs morizon-data ~/morizon-data -o nonempty -o iam_role='S3_permission_for_ec2' -o use_cache=/tmp -o allow_other -o mp_umask=077 -o uid=1000 -o gid=1000
 
 

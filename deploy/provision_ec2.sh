b239e9c1c1097eea95f59fe6a84e92001a77bd67
kabuboy <szczepanik.antoni@gmail.com>
2019-10-26 17:39:51 +0200

    add airflow deployment script

--- deploy/provision_ec2.sh
+++ deploy/provision_ec2.sh
@@ -7,6 +7,10 @@ curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 sudo usermod -aG docker ubuntu
 
+# compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 # install make and run container
 sudo apt install make
 cd flats && \

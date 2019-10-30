6bbeb5f8a82fc740a28ce17a4b38ec87914d4139
antoniszczepanik <antoni.szczepanik@greensteam.com>
2019-10-30 18:13:26 +0100

    change name in scrapy task

--- airflow/deps/pipelines/scrape_task.py
+++ airflow/deps/pipelines/scrape_task.py
@@ -7,7 +7,7 @@ from scrapy.utils.project import get_project_settings
 from spider.morizon_spider.spiders.morizon_spider import MorizonSpider
 from spider.morizon_spider.spiders.morizon_spider_rent import MorizonSpiderRent
 
-SPIDER_MAP = {
+spiders = {
     'sale': MorizonSpider,
     'rent': MorizonSpiderRent,
 }
@@ -17,7 +17,7 @@ def scrape_task(data_type):
     settings_file_path = 'spider.morizon_spider.settings' # The path seen from root, ie. from main.py
     os.environ.setdefault('SCRAPY_SETTINGS_MODULE', settings_file_path)
 
-    spider = SPIDER_MAP[data_type]
+    spider = spiders[data_type]
     process = CrawlerProcess(get_project_settings())
     process.crawl(spider)
 

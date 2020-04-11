0af7989bdf601a24c769243366cabd29c20e460b
antoniszczepanik <antoni.szczepanik@greensteam.com>
2020-04-11 18:45:16 +0200

    Make site a react app, add "create-react-app" like structure

---
+++ site/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Filter.css';
+
+function Filter() {
+  return (
+    <div className="Filter">
+      <p>
+        "heres filter p tag"
+      </p>
+    </div>
+  );
+}
+
+export default App;

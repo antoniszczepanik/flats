9ec5b3f8dfcd420d84394caa946201a4d1079017
antoniszczepanik <antoni.szczepanik@greensteam.com>
2020-04-13 18:59:37 +0200

    USE-REACT: Add a form component with autoupdating state

--- site/src/Filter.js
+++ site/src/Filter.js
@@ -4,32 +4,49 @@ import './Filter.css';
 //Todo: Something is no yes - cannot read state in fuctnion
 //Should use controlled components...
 class Filter extends Component {
-  state = {
-    size_min: 10,
-    size_max: 40,
-    transaction: 'wynająć',
-    city: 'Warszawie',
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      min_size: 40,
+      max_size: 50,
+      transaction: 'sale',
+      city: 'Warszawie',
+    };
+    this.handleMinSizeChange = this.handleMinSizeChange.bind(this);
+    this.handleMaxSizeChange = this.handleMaxSizeChange.bind(this);
+    this.handleTransactionChange = this.handleTransactionChange.bind(this);
+    this.handleCityChange = this.handleCityChange.bind(this);
+  }
 
-//  renderTransaction = () => {
-//    return {state.transaction};
-//  }
-//  rendeSize = () => {
-//    return {state.size_min};
-//  }
-//  renderCity = () => {
-//    return {state.city};
-//  }
+  handleTransactionChange(event) {
+    this.setState({transaction: event.target.value});
+  }
+  handleMaxSizeChange(event) {
+    this.setState({max_size: event.target.value});
+  }
+  handleMinSizeChange(event) {
+    this.setState({min_size: event.target.value});
+  }
+  handleCityChange(event) {
+    this.setState({city: event.target.value});
+  }
 
   render() {
     return (
-      <div>
+      <div id="filter">
         Chcę 
-//        {this.renderTransaction()}
+        <select id="transaction" value={this.state.transaction} onChange={this.handleTransactionChange}>
+          <option value="rent">wynajać</option>
+          <option value="sale">kupić</option>   
+        </select>
         mieszkanie wielkości
- //       {this.renderSize()} m2 w 
-  //      {this.renderCity()}.
+        <input id="min_size" type="text" value={this.state.min_size} onChange={this.handleMinSizeChange} />
+        -
+        <input id="max_size" type="text" value={this.state.max_size} onChange={this.handleMaxSizeChange} />
+        m2 w
+        <input id="city" type="text" value={this.state.city} onChange={this.handleCityChange} />
       </div>
+
     );
   };
 }

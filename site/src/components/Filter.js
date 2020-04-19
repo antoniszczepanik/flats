aad073422b3f2062c3a118f7410bd285d875267e
antoniszczepanik <antoni.szczepanik@greensteam.com>
2020-04-19 16:50:36 +0200

    USE-REACT: Use price instead of size in base filter, modify the prices accordingly to transaction type

--- site/src/components/Filter.js
+++ site/src/components/Filter.js
@@ -6,45 +6,59 @@ class Filter extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      min_size: 40,
-      max_size: 50,
+      min_price: 400,
+      max_price: 500,
       transaction: 'sale',
       city: 'Warszawa',
     };
-    this.handleMinSizeChange = this.handleMinSizeChange.bind(this);
-    this.handleMaxSizeChange = this.handleMaxSizeChange.bind(this);
+    this.handleMinPriceChange = this.handleMinPriceChange.bind(this);
+    this.handleMaxPriceChange = this.handleMaxPriceChange.bind(this);
     this.handleTransactionChange = this.handleTransactionChange.bind(this);
     this.handleCityChange = this.handleCityChange.bind(this);
   }
 
   handleTransactionChange(event) {
-    this.setState({transaction: event.target.value});
+    this.setState(
+      {
+      transaction: event.target.value,
+      min_price: event.target.value === 'sale' ? 400 : 2000,
+      max_price: event.target.value === 'sale' ? 600 : 3000,
+      }
+    );
   }
-  handleMaxSizeChange(event) {
-    this.setState({max_size: event.target.value});
+
+  handleMaxPriceChange(event) {
+    this.setState({max_price: event.target.value});
   }
-  handleMinSizeChange(event) {
-    this.setState({min_size: event.target.value});
+
+  handleMinPriceChange(event) {
+    this.setState({min_price: event.target.value});
   }
+
   handleCityChange(event) {
     this.setState({city: event.target.value});
   }
 
   render() {
     return (
-      <div id="filter">
+      <div className="filter">
         Chcę 
-        <select id="transaction" value={this.state.transaction} onChange={this.handleTransactionChange}>
+
+        <select className="transaction" value={this.state.transaction} onChange={this.handleTransactionChange}>
           <option value="rent">wynajać</option>
           <option value="sale">kupić</option>   
         </select>
-        mieszkanie wielkości
-        <input id="min_size" type="text" value={this.state.min_size} onChange={this.handleMinSizeChange} />
-        -
-        <input id="max_size" type="text" value={this.state.max_size} onChange={this.handleMaxSizeChange} />
-        m2 w
 
-        <select id="city" value={this.state.city} onChange={this.handleCityChange}>
+        mieszkanie za
+
+        <nobr>
+          <input className="min_price" type="text" value={this.state.min_price} onChange={this.handleMinPriceChange} />
+          -
+          <input className="max_price" type="text" value={this.state.max_price} onChange={this.handleMaxPriceChange} />
+          {this.formatCurrency()}zł w
+        </nobr>
+
+        <select className="city" value={this.state.city} onChange={this.handleCityChange}>
           <option value="Warszawa">Warszawie</option>   
           <option value="Kraków">Krakowie</option>
           <option value="Gdańsk">Gdańsku</option>
@@ -54,6 +68,12 @@ class Filter extends Component {
 
     );
   };
+
+  formatCurrency() {
+    const { transaction } = this.state;
+    return transaction === 'sale' ? 'tysięcy ' : '';
+  };
+
 }
 
 export default Filter;

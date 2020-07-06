7eadee516419b1a741f8e7bdbbd8b39666f88787
antoniszczepanik <antoni.szczepanik@greensteam.com>
2020-07-06 17:52:30 +0200

    USE-REACT: Add data flow and style all the components

--- site/src/components/FilterableFlatTable.js
+++ site/src/components/FilterableFlatTable.js
@@ -5,13 +5,66 @@ import './FilterableFlatTable.css';
 
 class FilterableFlatTable extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      min_price: 400,
+      max_price: 500,
+      transaction: 'sale',
+      city: 'Warszawa',
+    };
+    this.handleMinPriceChange = this.handleMinPriceChange.bind(this);
+    this.handleMaxPriceChange = this.handleMaxPriceChange.bind(this);
+    this.handleTransactionChange = this.handleTransactionChange.bind(this);
+    this.handleCityChange = this.handleCityChange.bind(this);
+  }
+
+  handleTransactionChange(event) {
+    this.setState(
+      {
+      transaction: event.target.value,
+      min_price: event.target.value === 'sale' ? 400 : 2000,
+      max_price: event.target.value === 'sale' ? 600 : 3000,
+      }
+    );
+  }
+
+  handleMaxPriceChange(event) {
+    this.setState({max_price: event.target.value});
+  }
+
+  handleMinPriceChange(event) {
+    this.setState({min_price: event.target.value});
+  }
+
+  handleCityChange(event) {
+    this.setState({city: event.target.value});
+  }
+
   render() {
     return (
       <div> 
-        <p> FilterableFlatTable </p>
-        <SearchBar/>
+        <SearchBar
+          transaction={this.state.transaction}
+          handleTransactionChange={this.handleTransactionChange}
+
+          min_price={this.state.min_price}
+          handleMinPriceChange={this.handleMinPriceChange}
+
+          max_price={this.state.max_price}
+          handleMaxPriceChange={this.handleMaxPriceChange}
+
+          city={this.state.city}
+          handleCityChange={this.handleCityChange}
+        />
         <br/>
-        <FlatsTable flats={this.props.flats} />
+        <FlatsTable 
+          flats={this.props.flats}
+          transaction={this.state.transaction}
+          min_price={this.state.min_price}
+          max_price={this.state.max_price}
+          city={this.state.city}
+        />
       </div>
     );
   };
--- site/src/components/FlatRow.js
+++ site/src/components/FlatRow.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
 import './FlatRow.css';
 
+
 class FlatRow extends Component {
+  
+  getRandomEmoji() {
+    var emoji_array = [
+        '🤯', '💎', '😬', '🤔'
+    ];
+    let emoji = emoji_array[Math.floor(Math.random()*emoji_array.length)];
+    return emoji
+  };
 
   render() {
     return (
-      <div> 
-        {this.props.flat.title}
+      <div>
+        <hr className="flatRowRuler"></hr>
+        <tr className="flatRowRow"> 
+            <td className="flatRowSize"> <nobr>{this.props.flat.size}㎡</nobr></td>
+            <td>
+                <p className="flatRowTitle">
+                    {this.props.flat.title}
+                </p>
+                <p className="flatRowDescription"> 
+                    Warte około <span></span>
+                    <span className="flatRowPrice"> {this.props.flat.estimate} </span>
+                    zł, wystawione za <span></span>
+                    <span className="flatRowPrice"> {this.props.flat.price} </span>
+                    zł <span></span>
+                    {this.getRandomEmoji()}
+                </p>
+            </td>
+        </tr>
       </div>
 
     );

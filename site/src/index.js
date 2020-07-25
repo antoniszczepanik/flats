f970dcaff15370ddfc4d63d56d450c9dc9e02dba
Antoni Szczepanik <antoni.szczepanik@greensteam.com>
2020-07-25 12:53:37 +0200

    Read flats data from public folder

--- site/src/index.js
+++ site/src/index.js
@@ -4,14 +4,25 @@ import './index.css';
 import FilterableFlatTable from './components/FilterableFlatTable';
 import * as serviceWorker from './serviceWorker';
 
-import data from './data';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <FilterableFlatTable flats={data} />
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+// "hack" with XML request is used to use json data from a public folder
+var xhttp = new XMLHttpRequest();
+var data = {};
+xhttp.onreadystatechange = function() {
+    if (this.readyState === 4 && this.status === 200) {
+      // Typical action to be performed when the document is ready:
+      data = JSON.parse(xhttp.responseText);
+      ReactDOM.render(
+        <React.StrictMode>
+          <FilterableFlatTable flats={data} />
+        </React.StrictMode>,
+        document.getElementById('root'),
+      );
+    }
+};
+
+xhttp.open("GET", `${process.env.PUBLIC_URL}/data.json`, true);
+xhttp.send();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

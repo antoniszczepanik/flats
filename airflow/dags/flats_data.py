4d849b5a7439a568298ee22f3f735732e12e7731
Antoni Szczepanik <antoni.szczepanik@greensteam.com>
2020-08-02 18:51:40 +0200

    Uncomment publish website task

--- airflow/dags/flats_data.py
+++ airflow/dags/flats_data.py
@@ -62,9 +62,9 @@ for data_type in DATA_TYPES:
 
     scrape >> concat >> clean >> engineer_features >> apply_model
 
-#update_website_data = PythonOperator(
-#    task_id='update_website',
-#    python_callable=update_website_data_task,
-#    dag=dag)
-#
-#update_website_data
+update_website_data = PythonOperator(
+    task_id='update_website',
+    python_callable=update_website_data_task,
+    dag=dag)
+
+update_website_data

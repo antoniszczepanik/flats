ffbece555accd98031af251402d27b137e8f5f42
Antoni Szczepanik <antoni.szczepanik@greensteam.com>
2020-08-02 18:21:08 +0200

    Comment out update website data task

--- airflow/dags/flats_data.py
+++ airflow/dags/flats_data.py
@@ -61,10 +61,10 @@ for data_type in DATA_TYPES:
         dag=dag)
 
     scrape >> concat >> clean >> engineer_features >> apply_model
-
-update_website_data = PythonOperator(
-    task_id='update_website',
-    python_callable=update_website_data_task,
-    dag=dag)
-
-update_website_data
+#
+#update_website_data = PythonOperator(
+#    task_id='update_website',
+#    python_callable=update_website_data_task,
+#    dag=dag)
+#
+#update_website_data

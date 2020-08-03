63f8e93bfd32757b2da1e8c9677ecb3a82450387
Antoni Szczepanik <antoni.szczepanik@greensteam.com>
2020-08-03 21:01:14 +0200

    Separate thousands with space for sale offers

--- site/src/components/FlatRow.js
+++ site/src/components/FlatRow.js
@@ -4,8 +4,29 @@ import './FlatRow.css';
 
 class FlatRow extends Component {
   
-  formatPrice(number, transaction) {
-      return (transaction === 'sale' ? Math.round(number/1000)*1000 : Math.round(number/100)*100);
+  getSalePrices(flat){
+      let estimate = Math.round(flat.estimate/1000)*1000;
+      return (
+        <p className="flatRowDescription"> 
+            Warte ok.<span></span>
+            <span className="flatRowPrice"> {estimate.toLocaleString('en-US').replace(',', ' ')} </span>
+            zł, wystawione za <span></span>
+            <span className="flatRowPrice"> {flat.price.toLocaleString('en-US').replace(',', ' ')} </span>
+            zł <span></span>
+        </p>
+      )
+  }
+  getRentPrices(flat){
+      let estimate = Math.round(flat.estimate/100)*100;
+      return (
+        <p className="flatRowDescription"> 
+            Warte ok.<span></span>
+            <span className="flatRowPrice"> {estimate} </span>
+            zł, wystawione za <span></span>
+            <span className="flatRowPrice"> {flat.price} </span>
+            zł <span></span>
+        </p>
+      )
   }
 
   render() {
@@ -21,13 +42,7 @@ class FlatRow extends Component {
                 <p className="flatRowTitle">
                     <a href={this.props.flat.url}> {this.props.flat.title} </a>
                 </p>
-                <p className="flatRowDescription"> 
-                    Warte ok.<span></span>
-                    <span className="flatRowPrice"> {this.formatPrice(this.props.flat.estimate, this.props.transaction)} </span>
-                    zł, wystawione za <span></span>
-                    <span className="flatRowPrice"> {this.props.flat.price} </span>
-                    zł <span></span>
-                </p>
+                {this.props.transaction === 'sale' ? this.getSalePrices(this.props.flat) : this.getRentPrices(this.props.flat)}
             </td>
         </tr>
       </div>

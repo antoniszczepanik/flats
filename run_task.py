386e66591655f6b4d6fc4207b943a713fd22f465
Antoni <Antoni Szczepanik>
2020-10-04 23:21:57 +0200

    Remove airflow and run tasks in vanilla containers

--- run_task.py
+++ run_task.py
@@ -27,16 +27,16 @@ except ModuleNotFoundError as e:
     sys.exit(1)
 
 TASK_FUNCTIONS = {
-    "scrape": "from deps.pipelines.scrape_task import scrape_task; scrape_task('{offer_type}')",
-    "concat": "from deps.pipelines.concat_task import concat_data_task; concat_data_task('{offer_type}')",
-    "clean": "from deps.pipelines.cleaning_task import cleaning_task; cleaning_task('{offer_type}')",
-    "features": "from deps.pipelines.feature_engineering_task import feature_engineering_task; feature_engineering_task('{offer_type}')",
-    "apply": "from deps.pipelines.apply_task import apply_task; apply_task('{offer_type}')",
-    "update-data": "from deps.pipelines.update_website_data import update_website_data_task; update_website_data_task()",
+    "scrape": "from pipelines.scrape_task import scrape_task; scrape_task('{offer_type}')",
+    "concat": "from pipelines.concat_task import concat_data_task; concat_data_task('{offer_type}')",
+    "clean": "from pipelines.cleaning_task import cleaning_task; cleaning_task('{offer_type}')",
+    "features": "from pipelines.feature_engineering_task import feature_engineering_task; feature_engineering_task('{offer_type}')",
+    "apply": "from pipelines.apply_task import apply_task; apply_task('{offer_type}')",
+    "update-data": "from pipelines.update_website_data import update_website_data_task; update_website_data_task()",
     # on demand - not run on airflow
-    "coord-map": "from deps.pipelines.coords_map_task import coords_map_task; coords_map_task('{offer_type}')",
-    "unify-raw": "from deps.pipelines.unify_raw_task import unify_raw_data_task; unify_raw_data_task('{offer_type}')",
-    "monitor": "from deps.pipelines.monitor import monitor; monitor('{offer_type}')",
+    "coord-map": "from pipelines.coords_map_task import coords_map_task; coords_map_task('{offer_type}')",
+    "unify-raw": "from pipelines.unify_raw_task import unify_raw_data_task; unify_raw_data_task('{offer_type}')",
+    "monitor": "from pipelines.monitor import monitor; monitor('{offer_type}')",
 }
 
 if __name__ == "__main__":
@@ -59,13 +59,13 @@ if __name__ == "__main__":
     cwd = os.getcwd()
     home = str(pathlib.Path.home())
 
-    cmd.extend(["-v", f"{cwd}/airflow/dags:/usr/local/airflow/dags"])
-    cmd.extend(["-v", f"{cwd}/airflow/deps:/usr/local/airflow/deps"])
+    cmd.extend(["-v", f"{cwd}/code:/usr/local/code"])
     cmd.extend(["-v", f"{home}/.aws/credentials:/usr/local/airflow/.aws/credentials:ro"])
     if not args['--use-remote']:
         cmd.extend(["-e", "USE_MINIO=true"])
     cmd.extend(["--network=flats"])
-    cmd.extend(["airflow_webserver"])
+    cmd.extend(["--name=flats"])
+    cmd.extend(["flats"])
 
 
     python_command = TASK_FUNCTIONS[task]

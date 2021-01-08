d0b955961046c65647d3d5ad9a20356b934009ae
Antoni <Antoni Szczepanik>
2021-01-08 15:19:40 +0100

    Add script to upload sample data

---
+++ server/upload_sample_data.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import sys
+import requests
+
+import pandas as pd
+
+if len(sys.argv) == 1:
+    print("Sample usage:")
+    print("./upload_test_data.py rent test_file.csv")
+
+df = pd.read_csv(sys.argv[1])
+url = "http://localhost:8000/offers/"
+headers = {'Content-type': 'application/json'}
+
+for ix in df.index:
+    row_as_json = df.loc[ix].to_json()
+    response = requests.post(url, headers=headers, data=str(row_as_json))
+    print(response.text)

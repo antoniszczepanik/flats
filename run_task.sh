0c332e5ba69fc8c9e1e64758f59374caf13e90dd
Antoni <Antoni Szczepanik>
2021-01-19 21:38:49 +0100

    Delete all duplicates & redundant stuff after merge

--- run_task.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-docker run -it --rm \
-    -v "$PWD/code:/usr/local/code" \
-    -v "$HOME/.aws/credentials:/root/.aws/credentials:ro" \
-    --network=flats \
-    --name=flats \
-    antoniszczepanik/flats "$@"

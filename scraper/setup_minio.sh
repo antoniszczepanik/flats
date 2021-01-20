01fca184ac2e913ed4dd9be08409c06152e57a87
Antoni <Antoni Szczepanik>
2021-01-20 16:07:17 +0100

    Update readme and minor cleaning in scraper

--- scraper/setup_minio.sh
+++ scraper/setup_minio.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
 
-# Arguments:
-# $1 - host path to root directory of "s3" data
 mkdir -p ~/flats_buckets
 
 docker run --rm -itd \
--p 9999:9000 \
---name minio \
---user $(id -u):$(id -g) \
---network flats \
--e "MINIO_ACCESS_KEY=minio" \
--e "MINIO_SECRET_KEY=miniominio" \
--v ~/flats_buckets:/data \
-minio/minio -- server /data
-
+  -p 9999:9000 \
+  --name minio \
+  --user $(id -u):$(id -g) \
+  --network flats \
+  -e "MINIO_ACCESS_KEY=minio" \
+  -e "MINIO_SECRET_KEY=miniominio" \
+  -v ~/flats_buckets:/data \
+  minio/minio -- server /data

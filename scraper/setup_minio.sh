d8a4fe4c84173f970fea54c517a35f0b37eb2d17
Antoni <Antoni Szczepanik>
2021-01-22 15:02:23 +0100

    Change MinIO root to repo root

--- scraper/setup_minio.sh
+++ scraper/setup_minio.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-mkdir -p ~/flats_buckets
+mkdir -p $(pwd)/buckets/flats-data
+mkdir -p $(pwd)/buckets/flats-models
 
 docker run --rm -itd \
   -p 9999:9000 \
@@ -9,5 +10,5 @@ docker run --rm -itd \
   --network flats \
   -e "MINIO_ACCESS_KEY=minio" \
   -e "MINIO_SECRET_KEY=miniominio" \
-  -v ~/flats_buckets:/data \
+  -v $(pwd)/buckets:/data \
   minio/minio -- server /data
